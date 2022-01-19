ddc08a736cfb4b9b92e461a6ced85da9fe13c86e
anton <intemic@yandex.ru>
2022-01-19 22:05:02 +0500

    Декоратор

--- checker.py
+++ checker.py
@@ -1,7 +1,13 @@
+from flask import session
+from functools import wraps
+
+
 def check_logged_in(func) -> object:
-    def wrapper() -> str:
+    @wraps(func)
+    def wrapper(*args, **kwargs) -> str:
         if 'logged_in' in session:
-            return func()
+            return func(*args, **kwargs)
         else:
             return 'Вы НЕ в системе'
+
     return wrapper
