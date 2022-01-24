f95ac1adead53346f4fd6f250204c387470c7370
anton <intemic@yandex.ru>
2022-01-24 21:36:12 +0500

    Многопоточность

--- Chapter5/vsearch4web.py
+++ Chapter5/vsearch4web.py
@@ -1,7 +1,9 @@
-from flask import Flask, render_template, request, session
+from time import sleep
+from flask import Flask, render_template, request, session, copy_current_request_context
 from vsearch import search4letters
-from DBcm import UseDatabase, ConnectionError, CredentialsError
+from DBcm import UseDatabase, ConnectionError, CredentialsError, SQLError
 from checker import check_logged_in
+from threading import Thread
 
 app = Flask(__name__)
 
@@ -13,36 +15,29 @@ app.config['dbconfig'] = {"host": "127.0.0.1",
                           "database": "vsearchlogDB"}
 
 
-def log_request(req: "flask_request", res: str) -> None:
-    with open("vsearch.log", 'a') as log:
-        print(req.form, req.remote_addr, req.user_agent, res, file=log, sep='|')
-
-
-def log_db(req: "flask_request", res: str) -> None:
-    with UseDatabase(app.config['dbconfig']) as cursor:
-        _SQL = """insert into log
-                    (phrase, letters, ip, browser_string, results)
-                    values
-                    (%s, %s, %s, %s, %s)"""
-        cursor.execute(_SQL, (req.form["phrase"], req.form["letters"],
-                              req.remote_addr, req.user_agent.browser, res))
-
-
 @app.route('/search4', methods=["POST"])
 def do_search() -> 'html':
+    @copy_current_request_context
+    def log_request(req: "flask_request", res: str) -> None:
+        sleep(15)
+        with UseDatabase(app.config['dbconfig']) as cursor:
+            _SQL = """insert into log
+                        (phrase, letters, ip, browser_string, results)
+                        values
+                        (%s, %s, %s, %s, %s)"""
+            cursor.execute(_SQL, (req.form["phrase"], req.form["letters"],
+                                  req.remote_addr, req.user_agent.browser, res))
+
     phrase = request.form["phrase"]
     letters = request.form["letters"]
     title = "Ваши результаты"
     result = str(search4letters(phrase, letters))
-    log_request(request, result)
     try:
-        log_db(request, result)
-    except ConnectionError as err:
-        print(str(err))
-    except CredentialsError as err:
-        print(str(err))
+        t = Thread(target=log_request, args=(request, result))
+        t.start()
+        #log_request(request, result)
     except Exception as err:
-        print(str(err))
+        print('Не удалось обновить лог журнала')
 
     return render_template("result.html", the_title=title, the_phrase=phrase,
                            the_letters=letters, the_result=result)
@@ -65,12 +60,13 @@ def view_the_log() -> 'html':
             cursor.execute(_SQL)
             contents = cursor.fetchall()
     except ConnectionError as err:
-        print(str(err))
+        print('Подключение к БД присутствует?')
     except CredentialsError as err:
-        print(str(err))
+        print('Пользователь/пароль некорректен')
+    except SQLError as err:
+        print('Ваш запрос корректен?')
     except Exception as err:
         print(str(err))
-
 
     titles = ('Строка', 'Фраза', 'Ip адрес', ',Браузер', 'Результат')
     return render_template("viewlog.html",
